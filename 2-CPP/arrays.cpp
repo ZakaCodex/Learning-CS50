80b96e80b6336823adbea1c7d0469902fae4c9e3
ZakaCodeX <zaka.alariqi2008@gmail.com>
2026-08-23 02:50:12 +0300

    learning

--- 2-CPP/arrays.cpp
+++ 2-CPP/arrays.cpp
@@ -4,15 +4,14 @@ using namespace std;
 int main()
 {
 
-    int nums[4];
+    int nums[] = {10, 20, 30, 40};
 
-    nums[3] = 10;
-    nums[2] = 20;
-    nums[1] = 30;
-    nums[0] = 40;
+    // cout << "The first number in array is : " << nums[0] << "\n";
+    // cout << "The second number in array is : " << nums[1] << "\n";
+    // cout << "The third number in array is : " << nums[2] << "\n";
+    // cout << "The fourth number in array is : " << nums[3] << "\n";
 
-    cout << "The first number in array is : " << nums[0] << "\n";
-    cout << "The second number in array is : " << nums[1] << "\n";
-    cout << "The third number in array is : " << nums[2] << "\n";
-    cout << "The fourth number in array is : " << nums[3] << "\n";
+    cout << sizeof(nums) / sizeof(nums[0]) << endl;
+
+    return 0;
 }
